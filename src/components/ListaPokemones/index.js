f90d771b1bf8001052de221c96331a38f3198d69
DanieÃl Cu <danielcu@alternet.com.mx>
2020-11-03 19:45:48 -0600

    Bootstrap y componente pokemon

--- src/components/ListaPokemones/index.js
+++ src/components/ListaPokemones/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Pokemon } from '../Pokemon';
 
 export const ListaPokemones = ()=>{
-    const pokemones = [ {id: 1, nombre: 'Pokemon 1'}, {id: 2, nombre: 'Pokemon 2'}, {id: 3, nombre: 'Pokemon 3'},{id: 4, nombre: 'Pokemon 4'}];
+    const pokemones = [ {id: 1, nombre: 'Pokemon 1', nivelPoder: 10 }, {id: 2, nombre: 'Pokemon 2', nivelPoder: 8}, {id: 3, nombre: 'Pokemon 3', nivelPoder: 6},{id: 4, nombre: 'Pokemon 4', nivelPoder: 5}];
     return(
-        <>
+        <div className="container">
             <h1>Lista de pokemones</h1>
-            {
-                pokemones.map(pokemon => <Pokemon key={pokemon.id} nombre={pokemon.nombre} />)
-            }
-        </>
+
+            <div className="row">
+                {
+                    pokemones.map(pokemon => <Pokemon key={pokemon.id} nombre={pokemon.nombre} nivelPoder={ pokemon.nivelPoder} />)
+                }
+            </div>
+        </div>
     )
 }
--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 export const Pokemon = ( props )=>{
     console.log(props);
     return(
-        <h1>Nombre { props.nombre }</h1>
+        <div className="col-6 col-md-4">
+            <div class="card mt-4" >
+                <div class="card-body">
+                    <h5 class="card-title">{props.nombre}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">{props.nivelPoder}</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
