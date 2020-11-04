bcee1c2d82004ba910c17982bbb9b17075e98efc
DanieÃl Cu <danielcu@alternet.com.mx>
2020-11-03 20:35:20 -0600

    Fecth, useState, useEffect

--- src/components/ListaPokemones/index.js
+++ src/components/ListaPokemones/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Pokemon } from '../Pokemon';
 
 export const ListaPokemones = ()=>{
-    const pokemones = [ {id: 1, nombre: 'Pokemon 1', nivelPoder: 10 }, {id: 2, nombre: 'Pokemon 2', nivelPoder: 8}, {id: 3, nombre: 'Pokemon 3', nivelPoder: 6},{id: 4, nombre: 'Pokemon 4', nivelPoder: 5}];
+    const data = [ {id: 1, nombre: 'Pokemon 1', nivelPoder: 10 }, {id: 2, nombre: 'Pokemon 2', nivelPoder: 8}, {id: 3, nombre: 'Pokemon 3', nivelPoder: 6},{id: 4, nombre: 'Pokemon 4', nivelPoder: 5}];
+
+    const [ pokemones, setPokemones] = useState(data);
+
+    useEffect(()=>{
+        fetch('https://pokeapi.co/api/v2/pokemon')
+            .then(resp =>  resp.json())
+            .then( data => console.log(data.results))
+    },[])
+    //console.log(pokemones)
+
+    // setTimeout(()=>{
+    //     setPokemones([1,2,3,4,5,6])
+    // },2000)
+    
     return(
         <div className="container">
             <h1>Lista de pokemones</h1>
-
+    
             <div className="row">
                 {
                     pokemones.map(pokemon => <Pokemon key={pokemon.id} nombre={pokemon.nombre} nivelPoder={ pokemon.nivelPoder} />)
--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+
 export const Pokemon = ( props )=>{
-    console.log(props);
+    //console.log(props);
     return(
         <div className="col-6 col-md-4">
-            <div class="card mt-4" >
-                <div class="card-body">
-                    <h5 class="card-title">{props.nombre}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">{props.nivelPoder}</a>
+            <div className="card mt-4" >
+                <div className="card-body">
+                    <h5 className="card-title">{props.nombre}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">{props.nivelPoder}</a>
                 </div>
             </div>
         </div>
