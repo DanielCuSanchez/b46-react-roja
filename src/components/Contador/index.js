0b2604cf3768c150c011deb2c850afa2fd8d33a1
DanieÃl Cu <danielcu@alternet.com.mx>
2020-11-04 23:15:39 -0600

    drag and drop

--- src/components/Contador/index.js
+++ src/components/Contador/index.js
@@ -1,50 +1,83 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 export const Contador = () => {
 
-    useEffect(()=>{
-        console.log('Hola soy el useEffect SIN DEPENDECIAS')
-    })
+    const [contador, setContador ] = useState({
+        quatity: 0,
+        position: 'initial'
+    });
+    const { quatity, position } = contador;
 
-    useEffect(()=>{
-        console.log('Hola soy el useEffect con el ARREGLO VACIO')
-    },[])
-
-    const [contador, setContador ] = useState(0);
-
-    useEffect(()=>{
-        console.log('Hola soy el useEffect con DEPENCIA CONTADOR ')
-    },[contador]);
-
-    
     const handleIncrement = ()=>{
-        setContador( contador + 1);
+        setContador( {
+            ...contador,
+            quatity: quatity + 1
+        });
     }
 
     const handleDecrement = ()=>{
 
-        if( contador > 0){
-            setContador(contador - 1);
+        if( quatity > 0){
+            setContador({
+                ...contador,
+                quatity: quatity - 1
+            });
         }
         
     }
 
     const handleReset = ()=>{
-        setContador(0);
+        setContador({
+            ...contador,
+            quatity: 0
+        });
     }
 
-    const handleDrag = ( event )=>{
-        console.log(event)
+    const handleDrag = ( event, nombre )=>{
+        console.log(event.dataTransfer)
+        event.dataTransfer.setData("text/plain",nombre)
+        
+    }
+
+    const handleOnDragOver = (event) =>{
+        event.preventDefault();
+    } 
+    const handleOnDrop = (event, type)=>{
+        console.log(event.dataTransfer.getData("text"))
+        setContador({
+            ...contador,
+            position: type
+        })
+        event.dataTransfer.getData("text")
     }
 
     return (
-        <div className="container text-center">
-            <p onDrag={handleDrag} className="display-3">Contador = {contador}</p>
+        <div className="container">
+            <div className="container text-center" style={{height:'150px'}} onDrop={(e)=>handleOnDrop(e,'initial')} onDragOver={handleOnDragOver}>
+                Initial position
+                {
+                    position  === 'initial' && SeeCount(quatity, handleDrag)
+                }    
+            
+            </div> 
+            
             <div className="row justify-content-center">
                 <button onClick={handleIncrement} className="btn btn-primary">+1</button>  
                 <button onClick={handleDecrement} className="btn btn-danger">-1</button> 
                 <button onClick={handleReset} className="btn btn-warning">Reset</button> 
-            </div>            
+            </div>     
+            <div className="container bg-primary text-light text-center mt-5" style={{height:'150px'}} onDrop={(e)=>handleOnDrop(e,'newArea')} onDragOver={handleOnDragOver}>
+                Drop here
+                {
+                    position  === 'newArea' && SeeCount(quatity, handleDrag)   
+                }    
+            
+            </div>       
         </div>
+    )
+}
+
+const SeeCount = (contador, handleDrag) =>{
+    return (<h2 className="display-2" draggable onDragStart={(e)=>handleDrag(e,'titulo')}>Counter = {contador}</h2>
     )
 }
